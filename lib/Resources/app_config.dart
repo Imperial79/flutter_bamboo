4259fd9ad9f31e043ee16e3522c2037a0992fda9
Imperial79 <avishekverma79@gmail.com>
2025-01-27 21:14:00 +0530

    Bump app version to 1.1.3 and build number to 5

--- lib/Resources/app_config.dart
+++ lib/Resources/app_config.dart
@@ -1,5 +1,5 @@
-const String kAppVersion = "1.1.2";
-const String kAppBuild = "4";
+const String kAppVersion = "1.1.3";
+const String kAppBuild = "5";
 
 const String termsConditionLink = "";
 const String privacyPolicyLink = "";
