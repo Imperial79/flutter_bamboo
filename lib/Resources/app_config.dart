5c471cc5b48b8eeea7fad9421b76c4f3b891e8bd
Vivek <vivekdgp01@gmail.com>
2025-01-31 08:54:53 +0530

    Bump app version to 1.2.5 and build number to 8

--- lib/Resources/app_config.dart
+++ lib/Resources/app_config.dart
@@ -1,5 +1,5 @@
-const String kAppVersion = "1.2.4";
-const String kAppBuild = "7";
+const String kAppVersion = "1.2.5";
+const String kAppBuild = "8";
 
 const String termsConditionLink = "";
 const String privacyPolicyLink = "";
