0503eb97fbcd4126d217fca4532261651817bf9e
Imperial79 <avishekverma79@gmail.com>
2025-02-14 11:56:28 +0530

    Bump app version to 1.2.2 and update build number to 5

--- lib/Resources/app_config.dart
+++ lib/Resources/app_config.dart
@@ -1,5 +1,5 @@
-const String kAppVersion = "1.1.2";
-const String kAppBuild = "4";
+const String kAppVersion = "1.2.2";
+const String kAppBuild = "5";
 
 const String termsConditionLink = "https://ngforganic.com/privacy-policy.html";
 const String privacyPolicyLink = "https://ngforganic.com/privacy-policy.html";
