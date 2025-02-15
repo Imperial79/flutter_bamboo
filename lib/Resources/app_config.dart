846d02552594b09d50e148261ae256ead1eb1854
Vivek <vivekdgp01@gmail.com>
2025-02-15 19:50:41 +0530

    Bump app version to 1.2.3 and build number to 6

--- lib/Resources/app_config.dart
+++ lib/Resources/app_config.dart
@@ -1,5 +1,5 @@
-const String kAppVersion = "1.2.2";
-const String kAppBuild = "5";
+const String kAppVersion = "1.2.3";
+const String kAppBuild = "6";
 
 const String termsConditionLink = "https://ngforganic.com/privacy-policy.html";
 const String privacyPolicyLink = "https://ngforganic.com/privacy-policy.html";
