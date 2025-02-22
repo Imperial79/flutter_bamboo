ef49867fc9c745705839382ae1d4edd27d99bfe7
Vivek <vivekdgp01@gmail.com>
2025-02-22 16:45:28 +0530

    No code changes made; empty commit.

--- lib/Helper/pdf_invoice.dart
+++ lib/Helper/pdf_invoice.dart
@@ -113,12 +113,12 @@ class PdfInvoiceApi {
               children: [
                 Text(
                   "SOLD BY:",
-                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                  style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
                 ),
                 SizedBox(height: 10),
                 Text(
                   "NIGHTBIRDES HUB (OPC) PRIVATE LIMITED",
-                  style: const TextStyle(fontSize: 12),
+                  style: const TextStyle(fontSize: 10),
                 ),
                 Text(
                   """Floor No.: SECOND FLOOR
@@ -128,21 +128,21 @@ City/Town/Village: Chennai
 District: Chennai
 State: Tamil Nadu
 PIN Code: 600016""",
-                  style: const TextStyle(fontSize: 12),
+                  style: const TextStyle(fontSize: 10),
                 ),
                 SizedBox(height: 20),
                 Text(
                   "GST Registration No:33AAJCN0176A1ZY",
-                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                  style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
                 ),
                 SizedBox(height: 20),
                 Text(
                   "Order Number: ${orderDetails?.shoppingOrderId}",
-                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                  style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
                 ),
                 Text(
                   "Order Date: ${kDateFormat(orderDetails!.orderDate)}",
-                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                  style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
                 ),
               ],
             ),
@@ -154,27 +154,27 @@ PIN Code: 600016""",
               children: [
                 Text(
                   "BILLING/SHIPPING ADDRESS",
-                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                  style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
                   textAlign: TextAlign.right,
                 ),
                 SizedBox(height: 10),
                 Text(
                   "${orderDetails?.shippingName}",
-                  style: const TextStyle(fontSize: 12),
+                  style: const TextStyle(fontSize: 10),
                   textAlign: TextAlign.right,
                 ),
                 Text(
                   "${orderDetails?.shippingAddress}",
-                  style: const TextStyle(fontSize: 12),
+                  style: const TextStyle(fontSize: 10),
                   textAlign: TextAlign.right,
                 ),
                 Text(
                   "Place of supply:${orderDetails?.shippingState}",
-                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                  style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
                 ),
                 Text(
                   "Place of delivery:${orderDetails?.shippingState}",
-                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                  style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
                 ),
               ],
             ),
@@ -184,12 +184,83 @@ PIN Code: 600016""",
     );
   }
 
+  // static Widget itemsPart() {
+  //   double unitPrice = orderDetails!.salePrice - PdfInvoiceApi.taxAmount;
+  //   double netAmount =
+  //       orderDetails!.subTotal - (PdfInvoiceApi.taxAmount * orderDetails!.qty);
+  //   double taxPercent = orderDetails!.taxRate * 100;
+  //   double taxAmount = PdfInvoiceApi.taxAmount * orderDetails!.qty;
+  //   return Container(
+  //     width: double.infinity,
+  //     decoration: const BoxDecoration(
+  //       border: Border(
+  //         left: BorderSide(),
+  //         right: BorderSide(),
+  //         bottom: BorderSide(),
+  //       ),
+  //     ),
+  //     child: TableHelper.fromTextArray(
+  //       headers: [
+  //         'Sl. No.',
+  //         'Description',
+  //         'Unit Price',
+  //         'Qty',
+  //         'Net Amount',
+  //         'Tax Rate',
+  //         'Tax Amount',
+  //         'Discount',
+  //         'Total Amount'
+  //       ],
+  //       data: [
+  //         [
+  //           "1",
+  //           '${orderDetails?.name} - ${orderDetails?.attributeValue}',
+  //           kCurrencyFormat(unitPrice, decimalDigits: 2, symbol: ""),
+  //           '${orderDetails?.qty}',
+  //           kCurrencyFormat(netAmount, decimalDigits: 2, symbol: ""),
+  //           '${taxPercent.toStringAsFixed(1)}%',
+  //           kCurrencyFormat(taxAmount, decimalDigits: 2, symbol: ""),
+  //           kCurrencyFormat(orderDetails!.couponDiscount,
+  //               decimalDigits: 2, symbol: ""),
+  //           kCurrencyFormat(orderDetails!.netPayable,
+  //               decimalDigits: 2, symbol: ""),
+  //         ],
+  //         [
+  //           '',
+  //           Text('Total', style: TextStyle(fontWeight: FontWeight.bold)),
+  //           '',
+  //           '',
+  //           Text(kCurrencyFormat(netAmount, decimalDigits: 2, symbol: ""),
+  //               style: TextStyle(fontWeight: FontWeight.bold)),
+  //           '',
+  //           Text(kCurrencyFormat(taxAmount, decimalDigits: 2, symbol: ""),
+  //               style: TextStyle(fontWeight: FontWeight.bold)),
+  //           Text(
+  //               kCurrencyFormat(orderDetails!.couponDiscount,
+  //                   decimalDigits: 2, symbol: ""),
+  //               style: TextStyle(fontWeight: FontWeight.bold)),
+  //           Text(
+  //               kCurrencyFormat(orderDetails!.netPayable,
+  //                   decimalDigits: 2, symbol: ""),
+  //               style: TextStyle(fontWeight: FontWeight.bold))
+  //         ]
+  //       ],
+  //       headerStyle: TextStyle(fontWeight: FontWeight.bold),
+  //       headerDecoration: const BoxDecoration(color: PdfColors.grey300),
+  //       cellHeight: 30,
+  //       defaultColumnWidth: IntrinsicColumnWidth(),
+  //       cellAlignment: Alignment.centerLeft,
+  //     ),
+  //   );
+  // }
+
   static Widget itemsPart() {
     double unitPrice = orderDetails!.salePrice - PdfInvoiceApi.taxAmount;
     double netAmount =
         orderDetails!.subTotal - (PdfInvoiceApi.taxAmount * orderDetails!.qty);
     double taxPercent = orderDetails!.taxRate * 100;
     double taxAmount = PdfInvoiceApi.taxAmount * orderDetails!.qty;
+
     return Container(
       width: double.infinity,
       decoration: const BoxDecoration(
@@ -199,57 +270,91 @@ PIN Code: 600016""",
           bottom: BorderSide(),
         ),
       ),
-      child: TableHelper.fromTextArray(
-        headers: [
-          'Sl. No.',
-          'Description',
-          'Unit Price',
-          'Qty',
-          'Net Amount',
-          'Tax Rate',
-          'Tax Amount',
-          'Discount',
-          'Total Amount'
+      child: Table(
+        columnWidths: {
+          0: FixedColumnWidth(40), // Sl. No.
+          1: FixedColumnWidth(200), // Description (fixed width)
+          2: FixedColumnWidth(70), // Unit Price
+          3: FixedColumnWidth(50), // Qty
+          4: FixedColumnWidth(80), // Net Amount
+          5: FixedColumnWidth(50), // Tax Rate
+          6: FixedColumnWidth(80), // Tax Amount
+          7: FixedColumnWidth(80), // Discount
+          8: FixedColumnWidth(100), // Total Amount
+        },
+        border: TableBorder.all(),
+        children: [
+          TableRow(
+            decoration: const BoxDecoration(color: PdfColors.grey300),
+            children: [
+              textCell('Sl. No.', bold: true),
+              textCell('Description', bold: true),
+              textCell('Unit Price', bold: true),
+              textCell('Qty', bold: true),
+              textCell('Net Amount', bold: true),
+              textCell('Tax Rate', bold: true),
+              textCell('Tax Amount', bold: true),
+              textCell('Discount', bold: true),
+              textCell('Total Amount', bold: true),
+            ],
+          ),
+          TableRow(
+            children: [
+              textCell("1"),
+              textCell(
+                '${orderDetails?.name} - ${orderDetails?.attributeValue}',
+              ), // Truncate if too long
+              textCell(
+                  kCurrencyFormat(unitPrice, decimalDigits: 2, symbol: "")),
+              textCell('${orderDetails?.qty}'),
+              textCell(
+                  kCurrencyFormat(netAmount, decimalDigits: 2, symbol: "")),
+              textCell('${taxPercent.toStringAsFixed(1)}%'),
+              textCell(
+                  kCurrencyFormat(taxAmount, decimalDigits: 2, symbol: "")),
+              textCell(kCurrencyFormat(orderDetails!.couponDiscount,
+                  decimalDigits: 2, symbol: "")),
+              textCell(kCurrencyFormat(orderDetails!.netPayable,
+                  decimalDigits: 2, symbol: "")),
+            ],
+          ),
+          TableRow(
+            children: [
+              textCell(''),
+              textCell('Total', bold: true),
+              textCell(''),
+              textCell(''),
+              textCell(kCurrencyFormat(netAmount, decimalDigits: 2, symbol: ""),
+                  bold: true),
+              textCell(''),
+              textCell(kCurrencyFormat(taxAmount, decimalDigits: 2, symbol: ""),
+                  bold: true),
+              textCell(
+                  kCurrencyFormat(orderDetails!.couponDiscount,
+                      decimalDigits: 2, symbol: ""),
+                  bold: true),
+              textCell(
+                  kCurrencyFormat(orderDetails!.netPayable,
+                      decimalDigits: 2, symbol: ""),
+                  bold: true),
+            ],
+          ),
         ],
-        data: [
-          [
-            "1",
-            '${orderDetails?.name} - ${orderDetails?.attributeValue}',
-            kCurrencyFormat(unitPrice, decimalDigits: 2, symbol: "INR "),
-            '${orderDetails?.qty}',
-            kCurrencyFormat(netAmount, decimalDigits: 2, symbol: "INR "),
-            '${taxPercent.toStringAsFixed(1)}%',
-            kCurrencyFormat(taxAmount, decimalDigits: 2, symbol: "INR "),
-            kCurrencyFormat(orderDetails!.couponDiscount,
-                decimalDigits: 2, symbol: "INR "),
-            kCurrencyFormat(orderDetails!.netPayable,
-                decimalDigits: 2, symbol: "INR "),
-          ],
-          [
-            '',
-            Text('Total', style: TextStyle(fontWeight: FontWeight.bold)),
-            '',
-            '',
-            Text(kCurrencyFormat(netAmount, decimalDigits: 2, symbol: "INR "),
-                style: TextStyle(fontWeight: FontWeight.bold)),
-            '',
-            Text(kCurrencyFormat(taxAmount, decimalDigits: 2, symbol: "INR "),
-                style: TextStyle(fontWeight: FontWeight.bold)),
-            Text(
-                kCurrencyFormat(orderDetails!.couponDiscount,
-                    decimalDigits: 2, symbol: "INR "),
-                style: TextStyle(fontWeight: FontWeight.bold)),
-            Text(
-                kCurrencyFormat(orderDetails!.netPayable,
-                    decimalDigits: 2, symbol: "INR "),
-                style: TextStyle(fontWeight: FontWeight.bold))
-          ]
-        ],
-        headerStyle: TextStyle(fontWeight: FontWeight.bold),
-        headerDecoration: const BoxDecoration(color: PdfColors.grey300),
-        cellHeight: 30,
-        defaultColumnWidth: IntrinsicColumnWidth(),
-        cellAlignment: Alignment.centerLeft,
+      ),
+    );
+  }
+
+// Helper function to create a text cell
+  static Widget textCell(String text, {bool bold = false, int maxLines = 5}) {
+    return Padding(
+      padding: const EdgeInsets.all(4),
+      child: Text(
+        text,
+        style: TextStyle(
+            fontWeight: bold ? FontWeight.bold : FontWeight.normal,
+            fontSize: 8),
+        maxLines: maxLines,
+        // overflow: TextOverflow.ellipsis,
       ),
     );
   }
@@ -266,9 +371,9 @@ PIN Code: 600016""",
       ),
       padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 2),
       child: Text(
-        "Amount in words:\n${AmountToWords().convertAmountToWords(orderDetails!.netPayable, ignoreDecimal: false)} ONLY",
+        "Amount in words: ${AmountToWords().convertAmountToWords(orderDetails!.netPayable, ignoreDecimal: false)} ONLY",
         style: TextStyle(
-          fontSize: 12,
+          fontSize: 10,
           fontWeight: FontWeight.bold,
         ),
       ),
@@ -291,7 +396,7 @@ PIN Code: 600016""",
         children: [
           Text(
             "For NIGHTBIRDES HUB (OPC) PRIVATE LIMITED:",
-            style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+            style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
           ),
           SizedBox(height: 10),
           Image(
@@ -305,7 +410,7 @@ PIN Code: 600016""",
           SizedBox(height: 10),
           Text(
             "Authorized Signatory",
-            style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+            style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
           ),
         ],
       ),
@@ -330,7 +435,7 @@ PIN Code: 600016""",
             padding: const EdgeInsets.symmetric(vertical: 10),
             child: Text(
               "Terms & Conditions:",
-              style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+              style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
             ),
           ),
           Text(
